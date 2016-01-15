5964d4308d32d3e8d03de4cf4aa368d6bad2396d
Yang Zhang <zhangyang@yunba.io>
2016-01-15 15:08:52 +0800

    covert float to integer

--- src/exometer_report_influxdb.erl
+++ src/exometer_report_influxdb.erl
@@ -344,7 +344,7 @@ key(K) ->
 
 -spec value(any()) -> binary() | list().
 value(V) when is_integer(V) -> [integer_to_binary(V), $i];
-value(V) when is_float(V) -> float_to_binary(V);
+value(V) when is_float(V) -> [float_to_binary(V, [{decimals, 0}]), $i];
 value(V) when is_atom(V) -> value(atom_to_binary(V, utf8));
 value(V) when is_list(V) -> value(list_to_binary(V));
 value(V) when is_binary(V) ->
